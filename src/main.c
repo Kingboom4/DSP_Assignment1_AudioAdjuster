cb5c3441c5e6cb1adcd8deec7d1e7f875da8a263
Wajahat Waraich <k26@hotmail.de>
2016-03-30 19:26:34 +0100

    main.c cleaned up
    Got rid of all the unwanted code in the AudioAdjuster example that was
    in the main.c file.

--- src/main.c
+++ src/main.c
@@ -79,144 +79,26 @@ OCPWMHandle 	*pOCPWMHandle 		= &ocPWMHandle;
 
 int main(void)
 {
-	int iMode = MODE_DO_NOTHING;
-	int iSwitch1Pressed = UNPRESSED;
-	int iSwitch2Pressed = UNPRESSED;
-	int iShiftAmount = 1;
-	
-	float fCarrierFrequency = 1;
-	createComplexSignal(fCarrierFrequency,FRAME_SIZE,compCarrierSignal);
-	
-	#ifdef __DEBUG_OVERRIDE_INPUT
-		float debugFrequency;
-	#endif
-
-	#ifdef __DEBUG_FILTERS//if in filter debug mode create a test signal
-		debugFrequency = 0;
-		createSimpleSignal(debugFrequency,FRAME_SIZE,frctAudioIn);
-	#endif
-
-	#ifdef __DEBUG_SHIFTERS//if in shifter debug mode create a constant test signal
-		debugFrequency = 1250;
-		createSimpleSignal(debugFrequency,FRAME_SIZE,frctAudioIn);		
-	#endif
-
-	#ifdef __DEBUG_TRANSFORMS//if in transform debug mode create a constant test signal
-		debugFrequency = 1250;
-		createSimpleSignal(debugFrequency,FRAME_SIZE,frctAudioIn);		
-	#endif
-	
 	initFilter();
-
-	ex_sask_init( );
-
+	
+	ex_sask_init();
+	
 	//Initialise Audio input and output function
 	ADCChannelInit	(pADCChannelHandle,adcBuffer);			
 	OCPWMInit		(pOCPWMHandle,ocPWMBuffer);			
-
+	
 	//Start Audio input and output function
 	ADCChannelStart	(pADCChannelHandle);
 	OCPWMStart		(pOCPWMHandle);	
-	
-	while(1)
-	{		
-		if(CheckSwitchS1() == PRESSED)//switch 1 is currently pressed
-		{
-			//switch 1 pressed causes a mode change
-			if(iSwitch1Pressed == UNPRESSED)//switch 1 has just transitioned from unpressed to pressed
-			{
-				iMode++;
-				if(iMode>=MODE_TOTAL)
-				{
-					iMode = 0;
-				}
-				iSwitch1Pressed = PRESSED;
-				fCarrierFrequency = 1;//mode change resets frequency shift
-				createComplexSignal(fCarrierFrequency,FRAME_SIZE,compCarrierSignal);//regenrate the carrier signal at reset frequency
-				iShiftAmount = 1;//mode change resets frequency shift
-				//displayMode(iMode);
-				#ifdef __DEBUG_FILTERS//if in debug mode reset the test signal
-					debugFrequency = 0;
-					createSimpleSignal(debugFrequency,FRAME_SIZE,frctAudioIn);		
-				#endif
-			}
-		}
-		else if(iSwitch1Pressed == PRESSED)//switch 1 has just transitioned from pressed to unpressed
-		{
-			iSwitch1Pressed = UNPRESSED;
-		}
-					
-		if(CheckSwitchS2() == PRESSED)//switch 2 is currently pressed  
-		{
-			//switch 2 is used to increase the carrier frequency
-			if(iSwitch2Pressed == UNPRESSED)//switch 2 has just transitioned from unpressed to pressed
-			{
-				iSwitch2Pressed = PRESSED;
-				
-				#ifndef __DEBUG_FILTERS//if not in debug mode adjust the carrier signal
-					if(fCarrierFrequency < UPPER_CARRIER_FREQ)//the carrier frequency has not reached the upper limit
-					{
-						fCarrierFrequency += CARRIER_INC;//increment carrier frequency by set amount						
-					}
-					if(iShiftAmount < 10)//the shift frequency has not reached the upper limit
-					{
-						iShiftAmount++;//increment the shift frequency for the Transform Mode
-					}					
-					createComplexSignal(fCarrierFrequency,FRAME_SIZE,compCarrierSignal);
-				#endif
-				
-				#ifdef __DEBUG_FILTERS//if in debug mode adjust the test signal
-					debugFrequency+=625;
-					createSimpleSignal(debugFrequency,FRAME_SIZE,frctAudioIn);		
-				#endif
-			}		
-		}
-		else if(iSwitch2Pressed == PRESSED)//switch 2 has just transitioned from pressed to unpressed
-		{
-			iSwitch2Pressed = UNPRESSED;
-		}
-		
-		#ifndef __DEBUG_OVERRIDE_INPUT//if not in debug mode, read audio in from the ADC
-			//Wait till the ADC has a new frame available
-			while(ADCChannelIsBusy(pADCChannelHandle));
-			//Read in the Audio Samples from the ADC
-			ADCChannelRead	(pADCChannelHandle,frctAudioIn,FRAME_SIZE);
-		#endif
 
-		switch(iMode)
-		{
-			case MODE_DO_NOTHING:
-			default:
-				VectorCopy(FRAME_SIZE,frctAudioOut,frctAudioIn);
-				break;
-			case MODE_BAND_PASS_FILTER: 
-				//filter out the audio signal's negative frequencies using the band pass filter
-				bandPassFilter(FRAME_SIZE,frctAudioOut,frctAudioIn);
-				break;
-			case MODE_LOW_PASS_FILTER:
-				//filter out the audio signal's negative frequencies using the shifted low pass filter 
-				shiftedLowPassFilter(FRAME_SIZE,frctAudioOut,frctAudioIn);
-				break;
-			case MODE_BAND_PASS_SHIFT: 
-				//filter out the audio signal's negative frequencies using the band pass filter
-				bandPassFilter(FRAME_SIZE,frctAudioWorkSpace,frctAudioIn);
-				//frequency shift the audio signal by multiplying it by the carrier signal 
-				combinationVectorMultiply(FRAME_SIZE,frctAudioOut,compAudioOut,frctAudioWorkSpace,compCarrierSignal);
-				break;
-			case MODE_LOW_PASS_SHIFT:
-				//filter out the audio signal's negative frequencies using the shifted low pass filter 
-				shiftedLowPassFilter(FRAME_SIZE,frctAudioWorkSpace,frctAudioIn);
-				//frequency shift the audio signal by multiplying it by the carrier signal 
-				combinationVectorMultiply(FRAME_SIZE,frctAudioOut,compAudioOut,frctAudioWorkSpace,compCarrierSignal);
-				break;
-			case MODE_FREQ_DOMAIN:
-				//work in the frequency domain
-				fourierTransform(FRAME_SIZE,compX,frctAudioIn);
-				filterNegativeFreq(FRAME_SIZE,compXfiltered,compX);
-				shiftFreqSpectrum(FRAME_SIZE,iShiftAmount,compXshifted,compXfiltered);
-				inverseFourierTransform(FRAME_SIZE,frctAudioOut,compXshifted);
-				break;			
-		}
+	while(1)
+	{
+		//work in the frequency domain
+		fourierTransform(FRAME_SIZE,compX,frctAudioIn);
+		filterNegativeFreq(FRAME_SIZE,compXfiltered,compX);
+		shiftFreqSpectrum(FRAME_SIZE,iShiftAmount,compXshifted,compXfiltered);
+		inverseFourierTransform(FRAME_SIZE,frctAudioOut,compXshifted);
+	
 		//Wait till the OC is available for a new frame
 		while(OCPWMIsBusy(pOCPWMHandle));	
 		//Write the real part of the frequency shifted complex audio signal to the output
